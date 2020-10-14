ce70350d4b287d0c60a1e8d614c1dc883bd8694e
baba230896 <yatish1996ss@gmail.com>
2020-10-14 10:57:49 +0530

    test env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,33 @@
 
 set -eo pipefail
 
+declare -a ENV_VARS
+
+[[ -n "${INPUT_YSQL_USER}" ]] && ENV_VARS+=(-e YSQL_USER="${INPUT_YSQL_USER}") || INPUT_YSQL_USER="yugabyte"
+
+[[ -n "${INPUT_YSQL_PASSWORD}" ]] && ENV_VARS+=(-e YSQL_PASSWORD="${INPUT_YSQL_PASSWORD}" -e PGPASSWORD="${INPUT_YSQL_PASSWORD}")
+
+[[ -n "${INPUT_YSQL_DB}" ]] && ENV_VARS+=(-e YSQL_DB="${INPUT_YSQL_DB}")
+
+[[ -n "${INPUT_YCQL_USER}" ]] && ENV_VARS+=(-e YCQL_USER="${INPUT_YCQL_USER}")
+
+[[ -n "${INPUT_YCQL_PASSWORD}" ]] && ENV_VARS+=(-e YCQL_PASSWORD="${INPUT_YCQL_PASSWORD}")
+
+[[ -n "${INPUT_YCQL_KEYSPACE}" ]] && ENV_VARS+=(-e YCQL_KEYSPACE="${INPUT_YCQL_KEYSPACE}")
+
 SERVICE_ID=$(docker run \
   -p "${INPUT_YB_MASTER_UI_PORT}:${INPUT_YB_MASTER_UI_PORT}" \
   -p "${INPUT_YB_TSERVER_UI_PORT}:${INPUT_YB_TSERVER_UI_PORT}" \
   -p "${INPUT_YSQL_PORT}:${INPUT_YSQL_PORT}" \
   -p "${INPUT_YCQL_PORT}:${INPUT_YCQL_PORT}" \
-  --detach \
+  --detach "${ENV_VARS[@]}" \
   yugabytedb/yugabyte:"${INPUT_YB_IMAGE_TAG}" \
   bin/yugabyted start --ycql_port="${INPUT_YCQL_PORT}" --ysql_port="${INPUT_YSQL_PORT}" \
   --master_webserver_port="${INPUT_YB_MASTER_UI_PORT}" --tserver_webserver_port="${INPUT_YB_TSERVER_UI_PORT}" \
   --daemon=false --ui=false)
 
 function waitUntilHealthy() {
-  while ! docker exec "$1" ysqlsh -p "$2" -c \\conninfo; do
+  while ! docker exec "$1" ysqlsh -U "${INPUT_YSQL_USER}" -p "$2" -c \\conninfo; do
     sleep 5s
   done
 }
